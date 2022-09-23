761ec3a867dc1d04063d9f83ebb5ff74899355ad
kovesdieszter <kovesdieszter@github.com>
2022-09-23 12:50:32 +0200

    feat: implement choose date from a calendar feature

--- asknasa/src/component/main.js
+++ asknasa/src/component/main.js
@@ -1,24 +1,36 @@
 import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
 
-
-const fetchData = async () => {
-    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=Txc7ZwMQNMBcQn9tWVpPKlsDRbbW02ZSDaAjG085");
-    return await response.json();
+const fetchData = async (previous) => {
+    console.log(previous)
+    if (previous === undefined) {
+        const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=Txc7ZwMQNMBcQn9tWVpPKlsDRbbW02ZSDaAjG085");
+        return await response.json();
+    } else {
+        const response = await fetch("https://api.nasa.gov/planetary/apod/?api_key=Txc7ZwMQNMBcQn9tWVpPKlsDRbbW02ZSDaAjG085&date=" + previous);
+        return await response.json();
+    }
 }
 
 
 const MainPageComponent = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [state, setState] = useState("");
     useEffect(() => {
-        fetchData().then((data) => {
+        fetchData(state).then((data) => {
             setData(data)
         })
-    }, [])
+    }, [state]);
     return (
         <div>
-            <h1> {data.title} </h1>
-            <img  style={{"height" : "auto", "width" : "100%"}} src={data.hdurl}/>
-            <p> {data.explanation}</p>
+            <h1>Astronomy Picture of the Day</h1>
+            <label htmlFor="previous">Choose date: </label>
+            <input type="date" id="previous" name="previous"
+                   onChange={previous => setState(previous.target.value)}
+                   max="<?= date('Y-m-d'); ?"
+            ></input>
+            <h2> {data.title} </h2>
+            <img  style={{"height" : "auto", "width" : "800px"}} src={data.hdurl}/>
         </div>
     );
 }
